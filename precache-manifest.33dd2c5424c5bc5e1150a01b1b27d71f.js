2c53f4eb7677d202d494922ae2078ef39e833838
Alexandr <Jekyns@yandex.ru>
2020-08-07 02:43:18 +0300

    deploy sendsay-exercise

---
+++ precache-manifest.33dd2c5424c5bc5e1150a01b1b27d71f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7785233d1b4a3c677281d288147f23e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45876223d2b03d0a0ad5",
+    "url": "/static/css/main.be42e755.chunk.css"
+  },
+  {
+    "revision": "4e1278b79ebbb6917208",
+    "url": "/static/js/2.8615e393.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/static/js/2.8615e393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45876223d2b03d0a0ad5",
+    "url": "/static/js/main.853e99c7.chunk.js"
+  },
+  {
+    "revision": "ae799a090315342e9522",
+    "url": "/static/js/runtime-main.0f2c5188.js"
+  },
+  {
+    "revision": "983a8344f362dae66bfbd6574d72ade5",
+    "url": "/static/media/Spinner.983a8344.gif"
+  },
+  {
+    "revision": "dffa408b6c7eb78a5e0c368ca2fda552",
+    "url": "/static/media/dots-black.dffa408b.svg"
+  },
+  {
+    "revision": "b55254f8e335ab17a943282153f48052",
+    "url": "/static/media/dots.b55254f8.svg"
+  },
+  {
+    "revision": "9dd36fc16c8417014f6b31e7371033e7",
+    "url": "/static/media/logo.9dd36fc1.svg"
+  },
+  {
+    "revision": "b920aea884089d0ddf871934dd798e42",
+    "url": "/static/media/small_logo.b920aea8.svg"
+  }
+]);
